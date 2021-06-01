ac41b85a4f394b1430b20b052d8c6fa9fcae3c9e
Orlando N. Rodriguez <dfashion.corp@gmail.com>
2021-06-01 08:01:37 -0400

    Add dependencies and some annotatios

--- app/src/main/java/com/ondev/lyricssearch/data/entity/Lyric.kt
+++ app/src/main/java/com/ondev/lyricssearch/data/entity/Lyric.kt
@@ -1,5 +1,11 @@
 package com.ondev.lyricssearch.data.entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "lyrics")
 data class Lyric(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
     val lyrics: String
 )
--- app/src/main/java/com/ondev/lyricssearch/data/source/local/ILyricsDao.kt
+++ app/src/main/java/com/ondev/lyricssearch/data/source/local/ILyricsDao.kt
@@ -1,4 +1,23 @@
 package com.ondev.lyricssearch.data.source.local
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.ondev.lyricssearch.data.entity.Lyric
+
+@Dao
 interface ILyricsDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(lyric: Lyric)
+
+    @Update
+    suspend fun update(lyric: Lyric)
+
+    @Query("SELECT * FROM lyrics ORDER BY id DESC")
+    fun getWallpapers(): LiveData<List<Lyric>>
+
+    @Query("DELETE FROM lyrics")
+    suspend fun deleteAll()
+
+    @Query("DELETE FROM lyrics WHERE id = :id")
+    suspend fun deleteByID(id: Int)
 }
